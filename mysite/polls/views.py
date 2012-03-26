449b5e805359cb65c12bf9437d31d4ef0501a4ef
Michael Palumbo <michael.palumbo87@gmail.com>
2012-03-26 21:36:08 +0200

    Added index.html template and index view is now decoupled from the page design.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
+from django.template import Context, loader
+from polls.models import Poll
 
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    t = loader.get_template('polls/index.html')
+    c = Context({
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(t.render(c))
+    
+# def index(request):
+#     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+#     output = ', '.join([p.question for p in latest_poll_list])
+#     return HttpResponse(output)
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the poll index.")
