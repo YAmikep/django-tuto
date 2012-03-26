bbfe1d88e92465a8f720d609bf7f80f07abe1c5c
Michael Palumbo <michael.palumbo87@gmail.com>
2012-03-26 15:07:58 +0200

    Assigned HTML classes to the 'Date information' fieldset.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        #('Date information', {'fields': ['pub_date']}),
     ]
     #fields = ['pub_date', 'question']
     
