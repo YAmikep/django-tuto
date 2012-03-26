04cbcb46b29ca8ceb7676f5c1218ee6c3c8ffc14
Michael Palumbo <michael.palumbo87@gmail.com>
2012-03-26 21:08:25 +0200

    Added hierarchical navigation by date.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -17,6 +17,7 @@ class PollAdmin(admin.ModelAdmin):
     list_display = ('question', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question']
+    date_hierarchy = 'pub_date'
     
 admin.site.register(Poll, PollAdmin)
 #admin.site.register(Choice)
