92c76f79c6d8bda98b48c2e0d5732b98f81efa64
Michael Palumbo <michael.palumbo87@gmail.com>
2012-03-26 22:13:06 +0200

    Modified vote view to actually do something.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 #from django.template import Context, loader
 from polls.models import Poll
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -12,6 +14,24 @@ def detail(request, poll_id):
     return render_to_response('polls/detail.html', {'poll': p},
                                context_instance=RequestContext(request))
     # return render_to_response('polls/detail.html', {'poll': p})
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render_to_response('polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
 
 # def detail(request, poll_id):
 #     try:
@@ -42,5 +62,5 @@ def detail(request, poll_id):
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
-def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+# def vote(request, poll_id):
+#     return HttpResponse("You're voting on poll %s." % poll_id)
