1947f16ce1cff52c5b593785a2eac2cf023bf048
Michael Palumbo <michael.palumbo87@gmail.com>
2012-03-26 15:04:56 +0200

    Added fieldsets for PollAdmin.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,11 @@ from polls.models import Poll
 from django.contrib import admin
 
 class PollAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question']
-
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    #fields = ['pub_date', 'question']
+    
 admin.site.register(Poll, PollAdmin)
 #admin.site.register(Poll)
