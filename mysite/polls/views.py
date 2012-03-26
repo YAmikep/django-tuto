c7a5d2946918e732f304a3447f0ff24047892ae5
Michael Palumbo <michael.palumbo87@gmail.com>
2012-03-26 21:41:49 +0200

    Added detail view and simple detail.html template.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,6 +7,13 @@ def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
+def detail(request, poll_id):
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p})
+    
 # def index(request):
 #     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
 #     t = loader.get_template('polls/index.html')
@@ -23,8 +30,8 @@ def index(request):
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the poll index.")
 
-def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+# def detail(request, poll_id):
+#     return HttpResponse("You're looking at poll %s." % poll_id)
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
