0427fa645013aba1c6f88a25a52e1e73a0ff3d04
Michael Palumbo <michael.palumbo87@gmail.com>
2012-03-26 21:38:34 +0200

    Use of the render_to_response shortcut in the view.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,20 @@
 from django.http import HttpResponse
-from django.template import Context, loader
+#from django.template import Context, loader
 from polls.models import Poll
-
+from django.shortcuts import render_to_response
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('polls/index.html')
-    c = Context({
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(t.render(c))
-    
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+
+# def index(request):
+#     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+#     t = loader.get_template('polls/index.html')
+#     c = Context({
+#         'latest_poll_list': latest_poll_list,
+#     })
+#     return HttpResponse(t.render(c))
+
 # def index(request):
 #     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
 #     output = ', '.join([p.question for p in latest_poll_list])
