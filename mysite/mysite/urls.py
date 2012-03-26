0dafe530590ba9b07e5f6e3904c5d64423321d4b
Michael Palumbo <michael.palumbo87@gmail.com>
2012-03-26 21:26:33 +0200

    Added polls urls.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -12,6 +12,11 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    url(r'^polls/$', 'polls.views.index'),
+    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
