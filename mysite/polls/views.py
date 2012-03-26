89b5cafd937b2eef3a744657cf2f86d3946830b8
Michael Palumbo <michael.palumbo87@gmail.com>
2012-03-26 21:42:56 +0200

    Use of the get_object_or_404 shortcut.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,19 +1,23 @@
 from django.http import HttpResponse
 #from django.template import Context, loader
 from polls.models import Poll
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    try:
-        p = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+    p = get_object_or_404(Poll, pk=poll_id)
     return render_to_response('polls/detail.html', {'poll': p})
-    
+
+# def detail(request, poll_id):
+#     try:
+#         p = Poll.objects.get(pk=poll_id)
+#     except Poll.DoesNotExist:
+#         raise Http404
+#     return render_to_response('polls/detail.html', {'poll': p})
+
 # def index(request):
 #     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
 #     t = loader.get_template('polls/index.html')
